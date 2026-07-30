7edb3c52093538a85643bd97c258891a19e2bed2
Tayane AS <asch.tayane@gmail.com>
2026-07-29 21:47:39 -0300

    build: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import cors from "cors";
 import { router } from "./routes/router.js";
+
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 app.listen(PORT, () => {
